7e4cd81abe37344e5d25cf05a77354c163daa8c7
Mystic Quest <kirimns@gmail.com>
2024-03-06 15:13:40 +0200

    Add db context factory for html scaffolding, Code Gen for scaffolding package, EF in memory to Models

---
+++ Models/CvManagementDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace CvManagementApp.Models
+{
+    public class CvManagementDbContextFactory : IDesignTimeDbContextFactory<CvManagementDbContext>
+    {
+        public CvManagementDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CvManagementDbContext>();
+            optionsBuilder.UseInMemoryDatabase("CvManagementDb");
+
+            return new CvManagementDbContext(optionsBuilder.Options);
+        }
+    }
+}
