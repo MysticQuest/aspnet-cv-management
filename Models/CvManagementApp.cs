0e70ee1ffe86f03de55afb3741fdaffd19c8764e
Mystic Quest <kirimns@gmail.com>
2024-03-06 14:19:57 +0200

    Add dbContext, EF package

---
+++ Models/CvManagementApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CvManagementApp.Models
+{
+    public class CvManagementDbContext : DbContext
+    {
+        public CvManagementDbContext(DbContextOptions<CvManagementDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Models/Degree.cs
+++ Models/Degree.cs
@@ -1,12 +1,4 @@
-﻿using CvManagementApp.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models
+﻿namespace CvManagementApp.Models
 {
     public class Degree : Entity
     {
